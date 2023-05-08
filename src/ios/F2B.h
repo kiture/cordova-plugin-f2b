0d1141822957d428cc6e04acc1343298e430eacd
kiture <kiture@gmail.com>
2023-05-08 20:30:00 +0200

    Update F2B.h

--- src/ios/F2B.h
+++ src/ios/F2B.h
@@ -9,12 +9,6 @@
 
 #endif /* F2B_h */
 #import <Cordova/CDVPlugin.h>
-#import <GCDWebServer/GCDWebServer.h>
-#import <GCDWebServer/GCDWebServerDataRequest.h>
-#import <GCDWebServer/GCDWebServerDataResponse.h>
-#import <GCDWebServer/GCDWebServerFileResponse.h>
-#import <GCDWebServer/GCDWebServerResponse.h>
-#import <GCDWebServer/GCDWebServerRequest.h>
 
 @interface F2B : CDVPlugin
 
