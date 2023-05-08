ec42c9b714e4fb94cc41eb8197d5806bd12d1dc0
kiture <kiture@gmail.com>
2023-05-08 19:56:19 +0200

    Add files via upload

--- src/ios/F2B.h
+++ src/ios/F2B.h
@@ -9,6 +9,12 @@
 
 #endif /* F2B_h */
 #import <Cordova/CDVPlugin.h>
+#import <GCDWebServer/GCDWebServer.h>
+#import <GCDWebServer/GCDWebServerDataRequest.h>
+#import <GCDWebServer/GCDWebServerDataResponse.h>
+#import <GCDWebServer/GCDWebServerFileResponse.h>
+#import <GCDWebServer/GCDWebServerResponse.h>
+#import <GCDWebServer/GCDWebServerRequest.h>
 
 @interface F2B : CDVPlugin
 
