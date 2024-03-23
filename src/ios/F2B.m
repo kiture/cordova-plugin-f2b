ab0599268ca166e8103bcd24f579e2f9371b3b73
kiture <kiture@gmail.com>
2024-03-23 09:16:37 +0100

    Update F2B.m

--- src/ios/F2B.m
+++ src/ios/F2B.m
@@ -19,6 +19,31 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+- (void)getRamTotalSize: (CDVInvokedUrlCommand *)command
+{
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt: ([NSProcessInfo processInfo].physicalMemory / 1024 / 1024)];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)getAvailableRamSize: (CDVInvokedUrlCommand *)command
+{
+    mach_port_t host_port;  
+    mach_msg_type_number_t host_size;  
+    vm_size_t pagesize;  
+    host_port = mach_host_self();  
+    host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);  
+    host_page_size(host_port, &pagesize);  
+    vm_statistics_data_t vm_stat;  
+    if (host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size) != KERN_SUCCESS)  
+    {  
+        NSLog(@"Failed to fetch vm statistics");  
+    }  
+    natural_t mem_free = vm_stat.free_count * pagesize; 
+
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt: (mem_free / 1024 / 1024)];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
 - (void)startServer: (CDVInvokedUrlCommand *)command
 {
     CDVPluginResult* pluginResult =  NULL;
